a3c7d589010fef353a856c0017d14d8697bf5ec2
Karun Dawadi <karun.dawadi@mavs.uta.edu>
2021-05-23 14:11:08 -0500

    Initial Commit

---
+++ dCash/dCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  dCashApp.swift
+//  dCash
+//
+//  Created by Karun Dawadi on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct dCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
